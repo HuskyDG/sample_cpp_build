6861f44a19fb037d155580ff4553af58295f96b4
Takeda-senpai <deadboltsx303@gmail.com>
2023-04-08 16:20:03 +0700

    ptrace

--- native/jni/Android.mk
+++ native/jni/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := example
-LOCAL_SRC_FILES := example.cpp
+LOCAL_MODULE := inject
+LOCAL_SRC_FILES := Inject.cpp
 LOCAL_STATIC_LIBRARIES := libcxx
 LOCAL_LDLIBS := -llog
 include $(BUILD_EXECUTABLE)
---
+++ native/jni/Utils/Logger.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <android/log.h>
+#include <errno.h>
+#define LOG_TAG "Injector"
+
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define PLOGE(fmt, args...) LOGE(fmt " failed with %d: %s\n", ##args, errno, std::strerror(errno))
