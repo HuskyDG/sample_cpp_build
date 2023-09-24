c324f06f19d482bb7cc23f6933d68969f0142f70
Takeda-senpai <deadboltsx303@gmail.com>
2023-09-24 18:59:40 +0700

    fake magisk

--- native/jni/Android.mk
+++ native/jni/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := example
-LOCAL_SRC_FILES := example.cpp
+LOCAL_MODULE := magisk
+LOCAL_SRC_FILES := main.cpp
 LOCAL_STATIC_LIBRARIES := libcxx
 LOCAL_LDLIBS := -llog
 include $(BUILD_EXECUTABLE)
